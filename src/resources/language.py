5b21836e1ba541be96934b8e90a8d8d5688b4a7b
Rafał Bielicki <bielickirafal93@gmail.com>
2017-09-18 20:40:39 +0200

    Added db model for Language text

--- src/resources/language.py
+++ src/resources/language.py
@@ -1,5 +1,6 @@
 from flask_restful import Resource, reqparse
 import requests
+from models.language import LanguageText
 
 
 class Language(Resource):
@@ -12,12 +13,6 @@ class Language(Resource):
 
     def post(self):
         data = Language.parser.parse_args()
-        text = data['text']
-
-        params = {'access_key': '12525a4a8f71d1952ad454c2df7061cb', "query": text}
-        languages = requests.get('http://apilayer.net/api/detect', params=params).json()
-
-        if languages['success']:
-            return languages
-        else:
-            return {"message": "Can't detect"}
+        lang = LanguageText(**data)
+        lang.save_to_db()
+        return lang.json(), 200
